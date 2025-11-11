3a98f1328d8b5445bc4460413dbac90a5df08fd9
michaeltheo <45629601+michaeltheo@users.noreply.github.com>
2025-11-11 21:33:06 +0200

    feat: add Radix UI components and integrate them into the application
    - Added new dependencies for Radix UI components including Dialog, Dropdown Menu, Tabs, Toast, and others.
    - Implemented a Button component with variant support.
    - Created Card component with header, footer, title, and description.
    - Developed Dialog component with overlay and close functionality.
    - Introduced Dropdown Menu component with various item types and submenus.
    - Added Input and Label components for form handling.
    - Implemented Tabs component for tabbed navigation.
    - Created Toast and Toaster components for displaying notifications.
    - Established a custom hook for managing toast notifications.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/ui";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,6 +25,7 @@ function App() {
         <p className="bg-green-500">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button onClick={() => alert("Button clicked!")}>ON PRESS</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
